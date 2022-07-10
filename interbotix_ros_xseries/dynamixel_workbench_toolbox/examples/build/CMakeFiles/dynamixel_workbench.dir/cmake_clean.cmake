058f777e6189f4895a8cecfcdcbf448cf200c1c6
butia <igormaurell@furg.br>
2022-07-10 01:20:57 -0300

    some changes

---
+++ interbotix_ros_xseries/dynamixel_workbench_toolbox/examples/build/CMakeFiles/dynamixel_workbench.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamixel_workbench.dir/home/butia/interbotix_ws/src/interbotix_ros_core/interbotix_ros_xseries/dynamixel_workbench_toolbox/src/dynamixel_workbench_toolbox/dynamixel_driver.cpp.o"
+  "CMakeFiles/dynamixel_workbench.dir/home/butia/interbotix_ws/src/interbotix_ros_core/interbotix_ros_xseries/dynamixel_workbench_toolbox/src/dynamixel_workbench_toolbox/dynamixel_item.cpp.o"
+  "CMakeFiles/dynamixel_workbench.dir/home/butia/interbotix_ws/src/interbotix_ros_core/interbotix_ros_xseries/dynamixel_workbench_toolbox/src/dynamixel_workbench_toolbox/dynamixel_tool.cpp.o"
+  "CMakeFiles/dynamixel_workbench.dir/home/butia/interbotix_ws/src/interbotix_ros_core/interbotix_ros_xseries/dynamixel_workbench_toolbox/src/dynamixel_workbench_toolbox/dynamixel_workbench.cpp.o"
+  "libdynamixel_workbench.a"
+  "libdynamixel_workbench.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamixel_workbench.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
